1b5d7ed4f4e2aeb02b7e4581b3e30f6e37941f08
Lucas Bordignon <lucaspbordignon99@gmail.com>
2017-05-26 08:34:44 -0300

    Using wget

--- data/collect_data.sh
+++ data/collect_data.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+#
+# This script intend to be just for manual download of the dataset. The main
+# jupyter-notebook downloads the dataset automatically.
 
 # Downloading the full dataset (3.3GB)
 echo "Downloading the dataset from Autti:"
-curl -O https://s3.amazonaws.com/udacity-sdc/annotations/object-dataset.tar.gz
+wget https://s3.amazonaws.com/udacity-sdc/annotations/object-dataset.tar.gz
 tar -xvf object-dataset.tar.gz
